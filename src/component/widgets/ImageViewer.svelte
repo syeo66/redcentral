461c8b7209379e0e0ccae5cd0f3b12f990a2a926
Red Ochsenbein <red@redochsenbein.ch>
2021-10-24 13:11:33 +0200

    Add preloader to imageviewer

--- src/component/widgets/ImageViewer.svelte
+++ src/component/widgets/ImageViewer.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   import PrevNext from '../PrevNext.svelte';
 
   interface Image {
@@ -8,13 +10,27 @@
 
   export let images: Image[] = [];
 
+  let isLoaded: boolean[] = images.map(() => false);
+
+  onMount(async () => {
+    images.forEach((image, key) => {
+      const img = new Image();
+      img.onload = () => (isLoaded[key] = true);
+      img.src = image.src;
+    });
+  });
+
   let pointer = 0;
   $: currentChart = images[pointer];
 </script>
 
 <div>
   <PrevNext max={images.length} bind:count={pointer} />
-  <img alt={currentChart.alt} src={currentChart.src} />
+  {#if isLoaded[pointer]}
+    <img alt={currentChart.alt} src={currentChart.src} />
+  {:else}
+    Loading...
+  {/if}
 </div>
 
 <style lang="scss">
@@ -24,5 +40,11 @@
     height: 100%;
     aspect-ratio: 1.619;
     border-radius: 0.5rem;
+  }
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
